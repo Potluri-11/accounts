f90990cc073dea94a6ff5630b9b90b4da2500481
Potluri-11 <srujanapotluri43@gmail.com>
2026-01-28 15:07:40 +0530

    Create CalculatorIT.java

---
+++ src/src/integrationTest/java/com/example/accounts/CalculatorIT.java
@@ -0,0 +1,12 @@
+package com.example.accounts;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CalculatorIT {
+
+    @Test
+    void integrationTestRuns() {
+        assertTrue(true);
+    }
+}
